0d92908d03a6fca5116631c0cdc5f1417f7780b4
Camila Alm <camila.salmeidatech@hotmail.com>
2024-09-19 03:36:27 -0400

    setup

---
+++ app.js
@@ -0,0 +1,8 @@
+const prompt = require('prompt-sync')();
+const username = prompt('What is your name');
+console.log(`Your name is ${username}`);
+
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+
