358e2146e3a34565549cf5af7515337a6931ddd2
Camila Alm <camila.salmeidatech@hotmail.com>
2024-09-19 23:08:51 -0400

    setup menu functions added

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const prompt = require('prompt-sync')();
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-//**------------------------------------------------------** 
+//**------------------------------------------------------------------------** 
 
 //Make the DATABASE CONNECTION  
 //Set up Mongoose and MongoDB Connection
@@ -14,16 +14,18 @@ const connect = async () => {
         //console.log('Connected to MOngoDB');
     await runQueries()
     await main()
+    //await createCustomer()
+    //await viewAllCustomers()
+    //await updateCustomer()
+    //await deleteCustomer()
+
     await mongoose.disconnect()
         //console.log('Disconnected from MongoDB')
     process.exit()
     }
 
 
-
 connect()
-
-
 /*------------------------------ Query Functions -----------------------------*/
 
 //Create the runQueries function 
@@ -34,18 +36,37 @@ const runQueries = async () => {
 //Create the main function which will display a Welcome Message showing a message in the terminal
 const main = async () => {
     console.log('Welcome to the CRM')
+    menu()
 }
 
 //Create the Menu Function
 const menu = async () => {
     console.log('What would you like to do?')
-    console.log("1. Create a customer")
-    console.log("2. View all customers")
-    console.log("3. Update a customer")
-    console.log("4. Delete a customer")
-    console.log("5. Quit")
+    console.log('1. Create a customer')
+    console.log('2. View all customers')
+    console.log('3. Update a customer')
+    console.log('4. Delete a customer')
+    console.log('5. quit')
 }
 
+//**------------------------CREATING THE MENU FUNCTIONS--------------------**
+
+const createCustomer = async () => {
+    console.log('Creating a customer')
+}
+
+
+const viewAllCustomers = async () => {
+    console.log('Viewing all customers')
+}
+
+const updateCustomer = async () => {
+    console.log('Updating a customer')
+}
+
+const deleteCustomer = async () => {
+    console.log('Deleting a customer')
+}
 
 
 //Import our model
